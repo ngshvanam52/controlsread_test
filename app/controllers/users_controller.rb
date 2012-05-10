5ecb94cc6272f889b82fb3a77bd830841e0c028a
ngshvanam52 <ngshvanam52@gmail.com>
2012-05-10 11:07:16 +0530

    sdf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,22 @@
 class UsersController < ApplicationController
 
-#require 'rubygems'
-#require File.dirname(__FILE__) + '/../../lib/typhoeus.rb'
-#require 'json'	
-
+require 'rubygems'
+require File.dirname(__FILE__) + '/../../lib/typhoeus.rb'
+require 'json'	
+#require 'open-uri'
   def new
 	#response = Typhoeus::Request.get("http://localhost:3000/index.html")
 	#@temp = response.body
   end
 
   def parse
-	#@url=params[:surl];
-	require 'open-uri'
-	body = open(params[:surl]).read
-	#response = Typhoeus::Request.get(params[:surl])
-	#@temp = response.body.gsub!("\n","")
-	@temp = body.gsub!("\n","")
-	puts "#{@temp}"
+	@url=params[:surl];
+	#require 'open-uri'
+	#body = open(params[:surl]).read
+	response = Typhoeus::Request.get(params[:surl])
+	@temp = response.body.gsub!("\n","")
+	#@temp = body.gsub!("\n","")
+	#puts "#{@temp}"
   end
 
 end
